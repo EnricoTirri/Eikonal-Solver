e14ff80daa20b53839564a055f8974f9bfaa243d
DarioSantoroDS <dario98.flash@gmail.com>
2023-12-18 15:14:15 +0000

    added new FMM parallel method

--- include/Methods.hpp
+++ include/Methods.hpp
@@ -24,7 +24,8 @@
 
 
 namespace methods {
-
+	constexpr double MAXF = 9000000;
+    
 	class ParallelStruct {
 	public:
 		size_t point;
@@ -35,6 +36,15 @@ namespace methods {
 
 
 	};
+
+
+
+    class PointElements{
+    public:
+        size_t punto;
+        std::vector<int> elements;
+        PointElements(size_t p):punto(p){}
+    };
 
 
 	class EikonalHeapComparator {
@@ -53,7 +63,6 @@ namespace methods {
     static bool FMM(std::vector<double> &U,
                     std::vector<int> X,
                     const Mesh<DIM, MESH_SIZE> &data) {
-	    constexpr double MAXF = 9000000;
 	    using Point = typename Eikonal_traits<DIM>::Point;
         for (auto &point: X) {
             if (point >= data.index.size()) {
@@ -170,7 +179,7 @@ namespace methods {
                             std::vector<int> X,
                             const Mesh<DIM, MESH_SIZE> &data)
     {
-	    constexpr double MAXF = 9000000;
+
 
 	    using Point = typename Eikonal_traits<DIM>::Point;
         for (auto &point : X)
@@ -316,6 +325,189 @@ namespace methods {
         std::cout << std::endl;
         return true;
     }
+
+
+    template <int DIM, int MESH_SIZE>
+    static bool FMMParallel2(std::vector<double> &U,
+                             std::vector<int> X,
+                             const Mesh<DIM, MESH_SIZE> &data)
+    {
+
+        typedef typename Eikonal_traits<DIM>::Point Point;
+        for (auto &point : X)
+        {
+            if (point >= data.index.size())
+            {
+                printf("error on initial point: %f %f does not belong to mesh\n", data.points[point].x(),
+                       data.points[point].y());
+                return false;
+            }
+        }
+
+        U.reserve(data.index.size());
+        std::priority_queue<int, std::vector<int>, EikonalHeapComparator>
+            minHeap((EikonalHeapComparator(U)));
+#pragma omp parallel for num_threads(4) schedule(static, 4)
+        for (int i = 0; i < data.points.size(); i++)
+        {
+            U[i] = MAXF;
+        }
+        for (const auto &i : X)
+        {
+            U[i] = 0;
+            minHeap.push(i);
+        }
+
+        int step_count = data.index.size() / 50;
+        int count = 0;
+        int step = 0;
+
+        std::vector<bool> L_set(data.points.size(), false);
+        std::vector<bool> L_in(data.points.size(), false);
+        while (!minHeap.empty())
+        {
+            int i = minHeap.top();
+            minHeap.pop();
+            L_set[i] = true;
+            ++count;
+            //   std::cout << count << " " << i << std::endl;
+            if (step_count == count - 1)
+            {
+                count = 0;
+                step += 2;
+                std::cout << '\r';
+                std::cout << step << "% ";
+                std::cout.flush();
+            }
+
+            // take time value of point p
+            double p = U[i];
+            // find neighbors of L[i] and get the base (the DIMENSION points with the smallest value of U
+            std::vector<int> neighbors;
+            std::size_t start, end;
+            start = data.index.at(i).start;
+            end = data.index.at(i).end;
+            for (std::size_t j = start; j < end; j++)
+            {
+                neighbors.push_back(data.adjacentList[j]);
+            }
+            bool error = false;
+
+            std::vector<PointElements> activepoints;
+
+            for (size_t index1 = 0; index1 < neighbors.size(); ++index1)
+            {
+                for (size_t index2 = 0; index2 < MESH_SIZE; ++index2)
+                {
+                    if(!L_in[data.elements[neighbors[index1]][index2]]){
+                        if (activepoints.empty())
+                        {
+                            PointElements activepoint(data.elements[neighbors[index1]][index2]);
+                            activepoint.elements.push_back(neighbors[index1]);
+                            activepoints.push_back(activepoint);
+                        }
+                        int inserted = 0;
+                        for (size_t index3 = 0; index3 < activepoints.size(); ++index3)
+                        {
+                            if (activepoints[index3].punto == data.elements[neighbors[index1]][index2])
+                            {
+                                activepoints[index3].elements.push_back(neighbors[index1]);
+                                ++inserted;
+                            }
+                        }
+                        if (!inserted)
+                        {
+                            PointElements activepoint(data.elements[neighbors[index1]][index2]);
+                            activepoint.elements.push_back(neighbors[index1]);
+                            activepoints.push_back(activepoint);
+                        }
+                    }
+                }
+            }
+            std::vector<ParallelStruct> parallelarray(activepoints.size());
+#pragma omp parallel for num_threads(4) schedule(dynamic)
+            for (size_t index1 = 0; index1 < activepoints.size(); ++index1)
+            {
+                const auto point = activepoints[index1].punto;
+                ParallelStruct p;
+                p.point = point;
+                p.value = U[point];
+                parallelarray[index1] = p;
+                for (size_t index2 = 0; index2 < activepoints[index1].elements.size(); ++index2)
+                {
+                    const auto m_element = activepoints[index1].elements[index2];
+                    if (point == i || L_set[point])
+                        continue;
+                    // if point in L continue
+                    // solve local problem with this point as unknown and the others as base
+                    std::array<Point, MESH_SIZE> base;
+                    std::size_t k = 0;
+                    Eigen::Matrix<double, MESH_SIZE, 1> values;
+                    for (std::size_t j = 0; j < MESH_SIZE; j++)
+                    {
+                        if ((data.elements[m_element])[j] == point)
+                        {
+                            base[MESH_SIZE - 1] = data.points[point];
+                        }
+                        else
+                        {
+                            base[k] = data.points[(data.elements[m_element])[j]];
+                            values[k] = U[(data.elements[m_element])[j]];
+                            k++;
+                        }
+                    }
+                    typename Eikonal::Eikonal_traits<DIM>::MMatrix M;
+                    if constexpr (DIM == 2)
+                        M << 1.0, 0.0,
+                            0.0, 1.;
+                    else if constexpr (DIM == 3)
+                        M << 1.0, 0.0, 0.0,
+                            0.0, 1.0, 0.0,
+                            0.0, 0.0, 1.0;
+                    Eikonal::SimplexData<DIM, MESH_SIZE> simplex{base, M};
+                    Eikonal::solveEikonalLocalProblem<DIM, MESH_SIZE> solver{std::move(simplex),
+                                                                             values};
+
+                    auto sol = solver();
+                    // if no descent direction or no convergence kill the process
+                    if (sol.status != 0)
+                    {
+                        printf("error on convergence\n");
+                        error = true;
+                        // continue;
+                    }
+                    p.value = sol.value;
+                    if (p.value < parallelarray[index1].value)
+                    {
+                        parallelarray[index1] = p;
+                    }
+                }
+            }
+            if (error)
+                return false;
+            for (size_t index1 = 0; index1 < activepoints.size(); ++index1)
+            {
+                if (U[parallelarray[index1].point] > parallelarray[index1].value)
+                {
+                    U[parallelarray[index1].point] = parallelarray[index1].value;
+                    Point p;
+                    for (int i = 0; i < DIM; i++)
+                    {
+                        p[i] = data.points[parallelarray[index1].point][i];
+                    }
+                    L_in[parallelarray[index1].point] = true;
+                    minHeap.push(parallelarray[index1].point);
+                }
+            }
+
+            activepoints.clear();
+        }
+
+        std::cout << std::endl;
+        return true;
+    }
+
+
                 //    //! Patch Mesh
                 //    template<int DIM, int MESH_SIZE>
                 //    static bool PM();

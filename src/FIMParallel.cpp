2f7f22b12d8df11b2447e00140fd4e06f7563528
Enrico Tirri <enrico.tirri@mail.polimi.it>
2024-05-25 13:13:30 +0200

    FIMP improvement, reduced thread spawning

--- src/FIMParallel.cpp
+++ src/FIMParallel.cpp
@@ -20,8 +20,6 @@ namespace Eikonal {
     template<int MESH_SIZE>
     inline void
     initialize(std::vector<double> &U, const std::vector<int> &X, const Mesh<MESH_SIZE> &data, std::vector<int> &active, const double &maxf) {
-#pragma omp parallel
-        {
             // Init values
 #pragma omp for
             for (int i = 0; i < U.size(); ++i)
@@ -39,7 +37,7 @@ namespace Eikonal {
                 U[pId] = 0;
                 int elRS = data.adjPointPtr[pId];
                 int elRE = data.adjPointPtr[pId + 1];
-#pragma omp parallel for
+//#pragma omp parallel for
                 for (int j = elRS; j < elRE; ++j) {
                     int eId = data.pointAdjacentElementList[j];
                     int pRS = data.adjElementPtr[eId];
@@ -50,7 +48,6 @@ namespace Eikonal {
                     }
                 }
             }
-        }
     }
 
     inline void applyScanAndPack(std::vector<int> &active, std::vector<int> &reducedActive) {
@@ -59,26 +56,25 @@ namespace Eikonal {
                 int stride = static_cast<int>(pow(2, i));
                 int step = 2 * stride;
 #pragma omp for simd
-                for (int k = step - 1; k < active.size(); k += step) {// it skip the last iteration because step is out of bounds of active, step +- 16M and active.size() =~ 13M
+                for (int k = step - 1; k < active.size(); k += step) {
                     active[k] += active[k - stride];
                 }
-#pragma omp barrier
             }
 
-            for (int i = std::log2(active.size()) - 1; i > 0; i--) {
+            for (int i = std::log2(active.size()); i > 0; i--) {
                 int stride = static_cast<int>(pow(2, i));
                 int step = stride / 2;
-#pragma omp for
+#pragma omp for simd
                 for (int k = stride - 1; k < active.size() - step; k += stride) {
                     active[k + step] += active[k];
                 }
-#pragma omp barrier
             }
-#pragma omp master //wouldn't single be better here? or do we need to specify the master thread?
+#pragma omp single
             {
                 reducedActive.resize(active.back());
             }
 #pragma omp barrier
+
 #pragma omp for
             for (int i = 0;i < active.size();++i) {
                 if (active[i] > 0) {
@@ -99,10 +95,9 @@ namespace Eikonal {
 
         newValues.resize(elRangeEnd - elRangeStart);
 
-#pragma omp parallel
-        {
-#pragma omp for
+
             for (int i = elRangeStart; i < elRangeEnd; i++) {
+
                 int elID = data.pointAdjacentElementList[i];
 
                 int pntRangeStart = data.adjElementPtr[elID];
@@ -125,20 +120,18 @@ namespace Eikonal {
                         0.0, 1.0, 0.0,
                         0.0, 0.0, 1.0;
 
-                LocalSolver<MESH_SIZE> solver(M, base, values, iter, 10e-6);
+                LocalSolver<MESH_SIZE> solver(M, base, values, iter, 10e-9);
                 double sol = solver();
                 newValues[i - elRangeStart] = sol;
             }
 
             for (int step = 1; step < newValues.size(); step *= 2) {
-#pragma omp for
                 for (int j = 0; j < newValues.size() - step; j += step) {
                     if (newValues[j] > newValues[j + step])
                         newValues[j] = newValues[j + step];
                 }
-#pragma omp barrier
             }
-        }
+
     }
 
 
@@ -162,14 +155,14 @@ namespace Eikonal {
         U.resize(data.points.size());
         active.resize(data.points.size());
         converged.resize(data.points.size());
+
+#pragma omp parallel
+        {
         initialize<MESH_SIZE>(U, X, data, active, MAXF);
         // ----------------------------------------------------------- //
 
         // ------------------- PACK OF ACTIVE LIST ------------------- //
-#pragma omp parallel // do parallel region work for code inside a function call?
-        {
             applyScanAndPack(active, reducedActive);
-        }
         // ----------------------------------------------------------- //
 
 
@@ -184,14 +177,13 @@ namespace Eikonal {
             std::cout << "Iteration: " << iteration << "\tActiveList size: " << active.size() << std::endl;
 #endif
 
-#pragma omp parallel
-            {
                 // --------------- UPDATE ALL ACTIVE AGGLOMERATES ----------------- //
-#pragma omp for
+#pragma omp for simd
                 for (int i = 0; i < active.size(); ++i) {
                     converged[i] = -1;
                     active[i] = 0;
                 }
+
 #pragma omp for
                 for (int i = 0; i < reducedActive.size(); ++i) {
                     std::vector<double> newValues;
@@ -216,7 +208,6 @@ namespace Eikonal {
 
                         int elRS = data.adjPointPtr[pId];
                         int elRE = data.adjPointPtr[pId + 1];
-#pragma omp parallel for
                         for (int j = elRS; j < elRE; ++j) {
                             int eId = data.pointAdjacentElementList[j];
                             int pRS = data.adjElementPtr[eId];
@@ -229,7 +220,7 @@ namespace Eikonal {
                     }
                 }
 
-#pragma omp for
+#pragma omp for simd
                 for (int i = 0; i < reducedActive.size(); ++i) {
                     int pId = reducedActive[i];
                     if(converged[i] > 0)
@@ -242,11 +233,12 @@ namespace Eikonal {
 
 
             // -------- CHECK IF ACTIVE AGGLOMERATES CONVERGE ----------------- //
-#pragma omp for
+#pragma omp for simd
                 for (int i = 0; i < active.size(); i++) {
                     converged[i] = -1;
                     active[i] = 0;
                 }
+
 #pragma omp for
                 for (int i = 0; i < reducedActive.size(); ++i) {
                     std::vector<double> newValues;
@@ -260,7 +252,7 @@ namespace Eikonal {
                     }
                 }
 
-#pragma omp for
+#pragma omp for simd
                 for (int i = 0; i < reducedActive.size(); ++i) {
                     int ptID = reducedActive[i];
                     if (converged[ptID] > 0)
@@ -273,6 +265,8 @@ namespace Eikonal {
             // ------------------- PACK OF ACTIVE LIST ------------------- //
             applyScanAndPack(active, reducedActive);
             // ----------------------------------------------------------- //
+
+//#pragma omp barrier
 
             }
         }
